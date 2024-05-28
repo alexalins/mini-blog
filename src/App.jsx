b4d5d6d0dc0e0c5f1c7cdd8b0fa80db4695134b7
alexalins <alexa@glinsoo.com>
2024-05-28 15:40:07 -0300

    Add routes e pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+//pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
 
 function App() {
 
   return (
     <div className='App'>
       <h1>Mini Blog</h1>
+      <BrowserRouter>
+        <div className='containor'>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
