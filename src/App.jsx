ce7e1f7bfdf2d6e5f811faf8a7e94c7a09161497
alexalins <alexa@glinsoo.com>
2024-05-30 14:53:14 -0300

    Ajuste de css nos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 //pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+//componentes
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -10,12 +13,14 @@ function App() {
     <div className='App'>
       <h1>Mini Blog</h1>
       <BrowserRouter>
-        <div className='containor'>
+      <Navbar/>
+        <div className='container'>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/about' element={<About/>}></Route>
           </Routes>
         </div>
+      <Footer/>
       </BrowserRouter>
     </div>
   )
