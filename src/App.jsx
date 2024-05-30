1e2347f5a6948ec52ff8fee6fcc96c9b1a927b6f
alexalins <alexa@glinsoo.com>
2024-05-30 15:14:31 -0300

    ajuste css e add pages de login e register

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 //pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
 //componentes
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+
 
 function App() {
 
   return (
     <div className='App'>
-      <h1>Mini Blog</h1>
       <BrowserRouter>
       <Navbar/>
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/register' element={<Register/>}></Route>
             <Route path='/about' element={<About/>}></Route>
           </Routes>
         </div>
