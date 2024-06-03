572c044f9c6d12b2a0a8e089e7d823e6721c44cb
alexalins <alexa@glinsoo.com>
2024-06-03 19:58:09 -0300

    iniciando pagina de criar post

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,11 +1,74 @@
 import styles from "./CreatePost.module.css";
 
-import React from 'react'
+import { useState } from "react";
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
 
-export default CreatePost
+  const hadleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.create_post}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={hadleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense em um bom título"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="imagem"
+            required
+            placeholder="Insira uma imagem que representa o seu post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+          <label>
+            <span>Tags:</span>
+            <input
+              type="text"
+              name="tags"
+              required
+              placeholder="Insira as tags separadas por vírgulas"
+              onChange={(e) => setTags(e.target.value)}
+              value={tags}
+            />
+          </label>
+        </label>
+        <button className="btn">Salvar</button>
+        {/*
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button className="btn" disabled>Aguarde...</button>}
+        { error && <p className="error">{error}</p>}*/}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
