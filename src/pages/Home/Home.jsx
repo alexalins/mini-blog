c9bb89287c934cfefa4a80b64aaed911086bb578
alexalins <alexa@glinsoo.com>
2024-06-04 20:22:45 -0300

    iniciando tela home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div className={styles.home}>Home</div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
 
-export default Home
+  const handleSubmit = () => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Busque por tags"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Post...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
